ec96d94c1eeade1b2c3d6acf90e677d682cb47db
Armand Bankhead <31142967+bankhead3@users.noreply.github.com>
2025-08-06 15:36:27 -0700

    Add files via upload

---
+++ pipelines/singleCell/02--processing-cell-cycle-regression/02-process.R
@@ -0,0 +1,119 @@
+# 20250620 arb
+
+library(Seurat)
+library(ggplot2)
+library(dplyr)
+library(harmony)
+library(dbscan)
+
+read. = F
+process. = F
+umap. = T
+cluster. = T
+plot. = T
+write. = T
+
+inFile1 = 'input/manifest.txt'
+inDir = 'input/sos/'
+outFile1 = 'intermediate/02.rds'
+outFilePrefix2 = 'figs/02-'
+project = 'lncap'
+
+data1 = read.delim(inFile1)
+
+if(read.) { 
+
+meta_cols = c('line','sample','sampleID')	
+files = dir(inDir)
+sos = list()
+for(file in files) {
+  sample = sub('.rds','',file)
+  inFile = paste0(inDir,file)  
+  so = readRDS(inFile)
+
+  # add metadata
+  for(meta_col in meta_cols) {
+    so@meta.data[[meta_col]] = data1[[meta_col]][data1$sample == sample]
+  }
+
+  # update cell names for uniqueness across datasets
+  so = RenameCells(so,add.cell.id = sample)
+
+  # qc involves creating separate umaps... need to re-iniitalize **
+  metaData = so@meta.data
+  counts = so@assays$RNA$counts
+  so = CreateSeuratObject(counts = counts,meta.data = metaData)
+
+  sos[[sample]] = so
+  print(sample)
+}
+}
+
+if(process.) { 
+
+  # we run this on each line separately since we are not interested in integration for this
+  # manuscript
+  for(c1 in 1:length(sos)) {
+    sample = names(sos)[c1]
+    print(sample)
+    so = sos[[c1]]
+
+    # process
+    so = NormalizeData(so)
+
+    options(warn=-1)
+    so = CellCycleScoring(so, s.features = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes, set.ident = TRUE)
+    options(warn=0)
+
+    # ** regress out cell cycle genes **
+    so = FindVariableFeatures(so, nfeatures = 3000)
+#    so = ScaleData(so)
+    so = ScaleData(so,vars.to.regress = c('S.Score','G2M.Score'), features = rownames(so))
+    # **
+
+    so = RunPCA(so, npcs = 20)
+    so = FindNeighbors(so, reduction = 'pca', dims = 1:20)
+    so = FindClusters(so, resolution = .1)  # initial default cluster resolution  
+    so = RunUMAP(so, reduction = 'pca', dims = 1:20, return.model = T) 
+    
+    # store it 
+    sos[[c1]] = so
+  }
+
+}
+
+if(umap.) { 
+
+
+  for(c1 in 1:length(sos)) {
+    sample = names(sos)[c1]
+    print(sample)
+    so = sos[[c1]]
+    so = FindClusters(so, resolution = .5)  # initial default cluster resolution  
+    so = RunUMAP(so, reduction = 'pca', dims = 1:20, return.model = T) #, min.dist = 0.1) # default min.dist = 0.3
+
+    # store it 
+    sos[[c1]] = so
+  }
+
+}
+
+if(plot.) { 
+  for(c1 in 1:length(sos)) {
+    sample = names(sos)[c1]
+    print(sample)
+    so = sos[[c1]]
+    set.seed(123)
+
+    # plot
+    metas = c('seurat_clusters','Phase') 
+    for(meta in metas) {
+      plot = DimPlot(so, reduction = 'umap', group.by = meta, pt.size = 1.5, raster = F, label = T)  # pt.size = 0.1
+      filename = paste0(outFilePrefix2,sample,'-dimplot-',meta,'.png')
+      ggsave(filename, plot, width = 11, height = 10, units = 'in', dpi = 600)
+      print(filename)
+    }
+  }
+}
+
+if(write.) { saveRDS(sos,outFile1); } 
